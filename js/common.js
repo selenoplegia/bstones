59eb771087f9e15bc755ecd354a0a9a97ce72527
sg-Lee <lksharky15@naver.com>
2022-11-02 13:19:17 +0900

    re

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 const content = document.querySelector('.content');
+const abc = document.querySelectorAll('.content li');
 const upside = document.querySelector('.upside');
 const triggerMenu = document.querySelector('.trigger-menu');
 const xm = document.querySelector('.xm');
@@ -19,16 +20,11 @@ function handleWindowSize() {
 window.addEventListener("resize", handleWindowSize);
 
 
-content.onclick = function () {
-    upside.classList.add('active');
-    cg.style.display = "none";
-    xm.style.display = "block";
-
-}
 bm.onclick = function () {
     upnav.classList.add('active');
     cg.style.display = "none";
     xm.style.display = "block";
+    content.classList.add('active');
 }
 
 triggerMenu.onclick = function () {
@@ -43,8 +39,10 @@ xm.onclick = function () {
         upside.classList.remove('active');
         cg.style.display = "block";
         xm.style.display = "none";
+        content.classList.remove('active');
     } else if (upnav.classList.contains('active')) {
         upnav.classList.remove('active');
+        content.classList.remove('active');
         cg.style.display = "block";
         xm.style.display = "none";
     } else {
@@ -54,69 +52,92 @@ xm.onclick = function () {
 }
 
 
-// $.ajax({
-//     url: "./data.json",
-//     success: function (data) {
+$.ajax({
+    url: "./data.json",
+    success: function (data) {
 
-//         let tags = '', name, category, date, client, concept, role, topimg;
+        
+        abc.forEach(function(ele,idx){
+            ele.onclick = function () {
+                upside.classList.add('active');
+                cg.style.display = "none";
+                xm.style.display = "block";
+                item(idx)
+            }
+        })
 
-//         $.each(data.items, function () {
-//             name = this.name
-//             date = this.data
-//             category = this.category
-//             client = this.client
-//             concept = this.concept
-//             role = this.role
-//             topimg = this.topimg
+        function item(n){
+            let tags = '', name, category, date, client, concept, role, topimg;
 
-//             console.log(category);
-//             tags = `<div class="content-ex2">
-//             <div class="mbh2">
-//                 <h2>${name}</h2>
-//             </div>
-//             <dl class="dl-box">
-//                 <dt><b>Category</b></dt>
-//                 <dd>
-//                     <span><img src="./img/icon_character01_off.gif" alt="카테고리"></span>
-//                     <span><img src="./img/icon_character02_off.gif" alt="카테고리"></span>
-//                     <span><img src="./img/icon_character03_on.gif" alt="카테고리"></span>
-//                     <span><img src="./img/icon_character04_off.gif" alt="카테고리"></span>
-//                 </dd>
-//             </dl>
-//         </div>
-//         <div class="content-ex3">
-//             <dl class="fl">
-//                 <dt><b>Date</b></dt>
-//                 <dd>${date}</dd>
-//             </dl>
-//             <dl class="fl">
-//                 <dt><b>Client</b></dt>
-//                 <dd>${client}</dd>
-//             </dl>
-//         </div>
-//         <div class="content-ex4">
-//             <dl>
-//                 <dt><b>Concept</b></dt>
-//                 <dd>${concept}</dd>
-//             </dl>
-//         </div>
-//         <div class="content-ex5">
-//             <dl>
-//                 <dt><b>Role</b></dt>
-//                 <ul>
-//                     <li>${role}</li>
-//                 </ul>
-//             </dl>
-//         </div>
-//     </div>
 
-//     <div class="middle-view">
-//         <div class="middle-img">
-//             <img src="${topimg}" alt="본죽">
-//         </div>
-//     </div>`;
 
-//         })
-//         $('.information').html(tags);
-//     }
-// })
+            $.each(data.items, function (i) {
+                name = this.name
+                date = this.date
+                category = this.category
+                client = this.client
+                concept = this.concept
+                role = this.role
+
+                if(n == i){
+                    tags = `<div class="content-ex2">
+                        <div class="mbh2">
+                            <h2>${name}</h2>
+                        </div>
+                        <dl class="dl-box">
+                            <dt><b>Category</b></dt>
+                            <dd>
+                                <span><img src="${category[0]}" alt="카테고리"></span>
+                                <span><img src="${category[1]}" alt="카테고리"></span>
+                                <span><img src="${category[2]}" alt="카테고리"></span>
+                                <span><img src="${category[3]}" alt="카테고리"></span>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div class="content-ex3">
+                        <dl class="fl">
+                            <dt><b>Date</b></dt>
+                            <dd>${date}</dd>
+                        </dl>
+                        <dl class="fl">
+                            <dt><b>Client</b></dt>
+                            <dd>${client}</dd>
+                        </dl>
+                    </div>
+                    <div class="content-ex4">
+                        <dl>
+                            <dt><b>Concept</b></dt>
+                            <dd>${concept}</dd>
+                        </dl>
+                    </div>
+                    <div class="content-ex5">
+                        <dl>
+                            <dt><b>Role</b></dt>
+                            <ul>
+                                ${role}
+                            </ul>
+                        </dl>
+                    </div>`
+                ;
+            }
+            })
+            $('.input').html(tags);
+        
+
+        $.each(data.items, function (j) {
+            topimg = this.topimg
+
+            if(n == j){
+                tags = `<div class="middle-view">
+                    <div class="middle-img">
+                        <img src="${topimg}" alt="본죽">
+                    </div>
+                </div>
+            </div>`
+            ;
+        }
+        })
+        $('.input2').html(tags);
+        }
+    }
+})
